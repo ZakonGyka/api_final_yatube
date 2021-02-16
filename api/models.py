d6fd5a1e42ad422c83fe6b3d3aa311b97910bdbc
Петр Лукахин <zakongyka@yandex.ru>
2021-02-17 01:27:29 +0300

    add group in models

--- api/models.py
+++ api/models.py
@@ -36,6 +36,18 @@ class Comment(models.Model):
     )
 
 
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+    )
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
 class Follow(models.Model):
     user = models.ForeignKey(
         User,
