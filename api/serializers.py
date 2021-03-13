6e7ffe268d9453ec1de3b208426b11d61693c2f2
Петр Лукахин <zakongyka@yandex.ru>
2021-03-13 17:47:08 +0300

    some fix serializers for review

--- api/serializers.py
+++ api/serializers.py
@@ -10,18 +10,18 @@ from api.models import User
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')
+    author = serializers.StringRelatedField(source='author.username')
 
     class Meta:
-        fields = ('id', 'text', 'author', 'pub_date', 'group')
+        fields = '__all__'
         model = Post
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')
+    author = serializers.StringRelatedField(source='author.username')
 
     class Meta:
-        fields = ('id', 'author', 'post', 'text', 'created')
+        fields = '__all__'
         model = Comment
 
 
